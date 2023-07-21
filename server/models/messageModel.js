f35065dcc8b628242d22477dacfa009d2bf1e7eb
Arman Abbasian <abasian.arman@gmail.com>
2023-07-21 08:05:32 +0330

    work on messages route on server side

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = mongoose.Schema(
-  {
+const MessageSchema = mongoose.Schema({
+  //body of message
     message: {
       text: { type: String, required: true },
     },
+    //message send form who to who
     users: Array,
+    //who send the message
     sender: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
